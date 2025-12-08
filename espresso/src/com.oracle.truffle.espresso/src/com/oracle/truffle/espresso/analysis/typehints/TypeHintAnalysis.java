b64727cc000a76fce8de27808b707c5cf29225b6
Jian Yu <j96yu@uwaterloo.ca>
2025-12-07 22:22:10 -0500

    filter methods before running analysis

--- espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/analysis/typehints/TypeHintAnalysis.java
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/analysis/typehints/TypeHintAnalysis.java
@@ -11,6 +11,9 @@ import com.oracle.truffle.espresso.runtime.EspressoContext;
 public class TypeHintAnalysis {
     public static TypeAnalysisResult[] analyze(Method.MethodVersion methodVersion) {
         Method method = methodVersion.getMethod();
+        if (!mayNeedAnalysis(method)) {
+            return null;
+        }
         Graph<? extends LinkedBlock> graph = GraphBuilder.build(method);
         EspressoContext context = method.getContext();
         int codeLength = method.getOriginalCode().length;
@@ -20,5 +23,15 @@ public class TypeHintAnalysis {
         TypePropagationClosure closure = new TypePropagationClosure(context, codeLength, methodVersion, maxLocals, maxStack, totalBlocks);
         BlockIterator.analyze(method, graph, closure);
         return closure.getRes();
+    }
+
+    private static boolean mayNeedAnalysis(Method method) {
+        if (method.getMethodParameterTypeAttribute() != null) {
+            return true;
+        }
+        if (method.getInvokeReturnTypeAttribute() != null) {
+            return true;
+        }
+        return false;
     }
 }
